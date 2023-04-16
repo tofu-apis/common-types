9a08a13a744ca849ff97e53ca84b76b4d3440a12
Kevin Fu <kevin.fu808@gmail.com>
2023-04-16 02:24:47 -0700

    Adding back export of NonEmptyArray

--- src/exclusive/nonEmptyArray.ts
+++ src/exclusive/nonEmptyArray.ts
@@ -1,4 +1,4 @@
-type NonEmptyArray<T> = [T, ...T[]];
+export type NonEmptyArray<T> = [T, ...T[]];
 
 function isNonZeroLengthArray<T>(array: T[]): array is NonEmptyArray<T> {
   return array.length > 0;
