73c31510cc36f49798d4d6d521be21398abad0df
Kevin Fu <kevin.fu808@gmail.com>
2023-04-16 02:01:55 -0700

    Updating nonEmptyArray to allow errorMessages

--- src/exclusive/nonEmptyArray.ts
+++ src/exclusive/nonEmptyArray.ts
@@ -1,19 +1,22 @@
-export type NonEmptyArray<T> = [T, ...T[]];
+type NonEmptyArray<T> = [T, ...T[]];
 
 function isNonZeroLengthArray<T>(array: T[]): array is NonEmptyArray<T> {
   return array.length > 0;
 }
 
-function requireArrayNonEmpty<T>(array: T[]): void {
+export function requireArrayNonEmpty<T>(
+  array: T[],
+  errorMessage?: string,
+): NonEmptyArray<T> {
   if (isNonZeroLengthArray(array)) {
-    return;
+    return array as NonEmptyArray<T>;
   }
 
-  throw new Error(`Input array ${array} must be non-empty.`);
+  throw new Error(
+    errorMessage === undefined
+      ? `Input array ${array} must be non-empty.`
+      : errorMessage,
+  );
 }
 
-export function castToNonEmptyArray<T>(inputArray: T[]): NonEmptyArray<T> {
-  requireArrayNonEmpty(inputArray);
-
-  return inputArray as NonEmptyArray<T>;
-}
+export default NonEmptyArray;
